bb06bca3bf56a9fde1c14f1c9b310748f9e62dc0
mehedy201 <mehedy643@gmail.com>
2022-10-23 18:39:35 +0600

    added SF Pro Display font

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div className="App-header">
-        
+        <h1 className='page_title'>Mehedi Hasan</h1>
       </div>
     </div>
   );
