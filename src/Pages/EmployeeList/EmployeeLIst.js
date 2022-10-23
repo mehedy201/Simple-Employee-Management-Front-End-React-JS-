07284c04464dc8a96294d1ad0c82bfe1dab7045a
mehedy201 <mehedy643@gmail.com>
2022-10-24 01:44:57 +0600

    complet full projects

--- src/Pages/EmployeeList/EmployeeLIst.js
+++ src/Pages/EmployeeList/EmployeeLIst.js
@@ -26,7 +26,7 @@ const EmployeeLIst = () => {
         console.log('Delete User')
     }
     return (
-        <div className='page_margin_top'>
+        <div className='page_margin_top h-screen'>
             <h2 className='page_title'>Employee List</h2>
             <button onClick={navigateAddEmployee} className='employee_list_page_button flex justify-center items-center'><AiOutlinePlus className='plus_icon' />Add new employee</button>
             <div>
@@ -48,12 +48,27 @@ const EmployeeLIst = () => {
                           <td className='td_padding flex'>
                               <button onClick={navigateEditEmployeePage} className='employee_list_btn flex'>Edit <BiEditAlt className='employee_list_table_icon'/></button>
                               <button onClick={navigateEmployeeDetailsPage} className='employee_list_btn flex'>Details <BiDetail className='employee_list_table_icon'/></button>
-                              <button onClick={deleteEmploye} className='employee_list_btn_delete flex'>Delete <RiDeleteBinLine className='employee_list_table_icon'/></button>
+                              <label htmlFor="my-modal" className='employee_list_btn_delete flex cursor-pointer'>Delete <RiDeleteBinLine className='employee_list_table_icon'/></label>
                               </td>
                         </tr>
                     </tbody>
                 </table>
             </div>
+
+            {/* Modal ----------------------------------------------------------- */}
+            <input type="checkbox" id="my-modal" className="modal-toggle" />
+            <div className="modal">
+              <div className="modal-box set_modal_width px-11">
+                <h3 className="pop_up_title text-center">Are you sure,you want to delete this employee?</h3>
+                <div className="modal-action">
+                  <div>
+                  <label onClick={deleteEmploye} htmlFor="my-modal" className="modal_button_1 flex justify-center items-center block cursor-pointer">Yes</label>
+                  <label htmlFor="my-modal" className="modal_button_2 flex justify-center items-center block cursor-pointer">No</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
         </div>
     );
 };
